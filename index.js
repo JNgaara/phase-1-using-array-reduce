c73dc149e179e527a6aada308555e31fa5468e62
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2024-12-04 22:47:06 +0200

    final add the total number of batteries

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 let totalBatteries = 0;
 // Code your solution here
-function reduceBatches(arr,testValue){
-    let newArray =[];
-    totalBatteries = arr.reduce(function(accumulator, element){
-        accumulator += element;
-        return accumulator
-    })
-}
+
 totalBatteries =batteryBatches.reduce((acc,curr)=> acc+curr,0); 
